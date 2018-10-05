a200c5a7ab2bdbb42ca04c55bf6bfd9277427bc0
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 17:35:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
    piglatin = PigLatinizer.new
-   @pig_latin = piglatin.Pparams[:user_phrase]
+   @pig_latin = piglatin.piglatinizeparams[:user_phrase]
    erb :results
   end
   
