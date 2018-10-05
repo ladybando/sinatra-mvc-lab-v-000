f37763d2bed1f15994f783ce0797f6948910f0f9
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 16:38:40 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,12 @@
 require 'pry'
 class PigLatinizer
 
-  def piglatinize(word)
-
+  def piglatinize(word) #word = "pork"
+  
   end
 
+  def piglatinize_vowels(char)
+  end
   # def sentence(sentence)
   #   sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   # end
