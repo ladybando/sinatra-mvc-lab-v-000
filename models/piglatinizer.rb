08b5d60607c6c5f327fadc7965e6be4f41466321
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 17:14:12 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,12 +4,7 @@ class PigLatinizer
   def piglatinize(word) #word = "pork"
    piglatinize_word(word)
   end
-  
-  
-  def consonants(char)
-    !char.match(/a e i o u/)
-  end
-  
+
   def piglatinize_word(word) #char = "pork"
     alpha = ('a'..'z').to_a
     vowels = %w[a e i o u]
