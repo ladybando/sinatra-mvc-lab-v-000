f9544ffa4303e28a370eb9213ef63d2469dce5bf
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 17:03:38 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,18 +11,16 @@ class PigLatinizer
   end
   
   def piglatinize_vowels(char) #char = "pork"
-    #binding.pry
     if !consonants(char)
       char = char + 'w'
     end
   end
   
   def piglatinize_consonants(char)
-        elsif consonants(char[0..1])
+    if consonants(char[0..1])
     binding.pry
     else 
       char.concat(char.slice!(/^[^aeiou]*/i || ""))
-    #char.concat(char.split(//).slice)
     end
     #char.split(//).slice(0).join(""),downcase #removes first letter and downcases it
     #will join first two letters with .join
