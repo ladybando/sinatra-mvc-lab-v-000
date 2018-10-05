670742dde03ca220230bc2fbb7795779851d4057
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 16:29:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::Base  get '/' do
+    erb :layout
+  end
+
+  post '/piglatinize' do
+   
+    erb :results
+  end
+  
 end
