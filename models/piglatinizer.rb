b9ce936a5cc154523da43f1dde6fd559ea839a23
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 17:28:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,17 +10,17 @@ class PigLatinizer
     vowels = %w[a e i o u]
     consonants = alpha - vowels
    
-    if vowels.include?(word[0].downcase)
-      word + 'way'
+      if vowels.include?(word[0].downcase)
+        word + 'way'
       elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
-        word[3..-1] + word[0..2] + 'ay'
-        elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
-          word[2..-1] + word[0..1] + 'ay'
-          elsif consonants.include?(word[0].downcase)
-            word[1..-1] + word[0] + 'ay'
-            else
-              word
-    end
+          word[3..-1] + word[0..2] + 'ay'
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
+        word[2..-1] + word[0..1] + 'ay'
+      elsif consonants.include?(word[0].downcase)
+        word[1..-1] + word[0] + 'ay'
+      else
+        word
+      end
    end
 
    def sentence(sentence)
