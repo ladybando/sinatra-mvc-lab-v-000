6ebeae564d8dccc6f7879331333119ebb15daab7
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 17:22:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,10 +14,10 @@ class PigLatinizer
     if vowels.include?(word[0].downcase)
       word + 'way'
       elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
-      word[3..-1] + word[0..2] + 'ay'
-      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
+        word[3..-1] + word[0..2] + 'ay'
+        elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
         word[2..-1] + word[0..1] + 'ay'
-      elsif consonants.include?(word[0].downcase)
+          elsif consonants.include?(word[0].downcase)
         word[1..-1] + word[0] + 'ay'
       else
       word
