db95868cc1b6847a44a93276351f0ee04faea1cd
Ayana Bando <ayanaflax@yahoo.com>
2018-10-05 17:13:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,15 +15,15 @@ class PigLatinizer
   vowels = %w[a e i o u]
   consonants = alpha - vowels
 
-  if vowels.include?(word[0])
-    word + 'way'
-  elsif consonants.include?(word[0]) && consonants.include?(word[1])
-    word[2..-1] + word[0..1] + 'ay'
-  elsif consonants.include?(word[0])
-    word[1..-1] + word[0] + 'ay'
-  else
-   word # return unchanged
-  end
+    if vowels.include?(word[0])
+      word + 'way'
+    elsif consonants.include?(word[0]) && consonants.include?(word[1])
+      word[2..-1] + word[0..1] + 'ay'
+    elsif consonants.include?(word[0])
+      word[1..-1] + word[0] + 'ay'
+    else
+     word # return unchanged
+    end
 end
 
   end
